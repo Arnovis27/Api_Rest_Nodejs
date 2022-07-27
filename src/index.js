073a2831970113fd274fd0eaa90b731aa73b9075
Arnovis27 <arnovisdee@hotmail.com>
2022-07-27 12:59:28 -0500

    Versionando rutas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const express= require("express");
-
+const v1Router= require("./v1/routes/");
 const app= express();
 const PORT= process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.send("<h1>Hello world<h1>");
-});
+//rutas
+app.use("/api/v1", v1Router);
 
 app.listen(PORT,()=>{
-    console.log("Server Listening in port ",PORT);
+    console.log(`Server Listening in port ${PORT}`);
 });
