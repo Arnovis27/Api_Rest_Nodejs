5801740952d674fbf5383c7e55bcb51fc7cbabbd
Arnovis27 <arnovisdee@hotmail.com>
2022-07-28 09:06:42 -0500

    service

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,27 @@
+const workoutService= require("../services/workoutService");
+
 const getAllWorkouts= (req,res)=>{
+    const allworkouts= workoutService.getAllWorkouts();
     res.send("Get All Workouts");
 };
 
 const getOneWorkout= (req,res)=>{
+    const workout= workoutService.getOneWorkout(req.params.workoutId);
     res.send(`Get Workout ${req.params.workoutId}`);
 };
 
 const createNewWorkout= (req,res)=>{
+    const createdworkout= workoutService.createNewWorkout(req.params.workoutId);
     res.send(`Create Workout ${req.params.workoutId}`);
 };
 
 const updateOneWorkout= (req,res)=>{
+    const updatedworkout= workoutService.updateOneWorkout(req.params.workoutId);
     res.send(`Update Workout ${req.params.workoutId}`);
 };
 
 const deleteOneWorkout= (req,res)=>{
+    workoutService.deleteOneWorkout(req.params.workoutId);
     res.send(`Delete Workout ${req.params.workoutId}`);
 };
 
---
+++ src/services/workoutService.js
@@ -0,0 +1,18 @@
+const getAllWorkouts= ()=>{
+    return;
+};
+
+const getOneWorkout= ()=>{
+    return;
+};
+const createNewWorkout= ()=>{
+    return;
+};
+
+const updateOneWorkout= ()=>{
+    return;
+};
+
+const deleteOneWorkout= ()=>{
+    return;
+};
